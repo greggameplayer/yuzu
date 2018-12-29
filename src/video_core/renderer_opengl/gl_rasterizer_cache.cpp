91f5caa0208bc6d35c64902bd7398ffb6628c1a8
greggameplayer <gregoire.hage@gmail.com>
2018-12-29 16:01:31 +0100

    recorrect texture1DBuffer

--- src/video_core/renderer_opengl/gl_rasterizer_cache.cpp
+++ src/video_core/renderer_opengl/gl_rasterizer_cache.cpp
@@ -545,7 +545,7 @@ CachedSurface::CachedSurface(const SurfaceParams& params)
                            format_tuple.internal_format, rect.GetWidth());
             break;
         case SurfaceTarget::Texture1DBuffer:
-            glTexBuffer(SurfaceTargetToGL(params.target), format_tuple.internal_format, 0);
+            glTexBuffer(GL_TEXTURE_BUFFER_EXT, format_tuple.internal_format, 0);
         case SurfaceTarget::Texture2D:
         case SurfaceTarget::TextureCubemap:
             glTexStorage2D(SurfaceTargetToGL(params.target), params.max_mip_level,
