510a996450ad7f7d4de0f648de1b4b671dd89763
ReinUsesLisp <reinuseslisp@airmail.cc>
2019-07-11 21:54:07 -0300

    kepler_compute: Implement texture queries

--- src/video_core/renderer_opengl/gl_rasterizer.h
+++ src/video_core/renderer_opengl/gl_rasterizer.h
@@ -145,6 +145,8 @@ private:
                       const Tegra::Texture::FullTextureInfo& texture,
                       const GLShader::SamplerEntry& entry);
 
+    void SetupComputeImages(const Shader& shader);
+
     /// Syncs the viewport and depth range to match the guest state
     void SyncViewport(OpenGLState& current_state);
 
