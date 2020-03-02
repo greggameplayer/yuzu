fd769df5aceea0e8d9096368d026117c216ae8f7
ReinUsesLisp <reinuseslisp@airmail.cc>
2020-03-02 19:31:26 -0300

    gl_rasterizer: Implement transform feedback bindings

--- src/video_core/engines/maxwell_3d.h
+++ src/video_core/engines/maxwell_3d.h
@@ -628,6 +628,11 @@ public:
             u32 address_low;
             s32 buffer_size;
             s32 buffer_offset;
+
+            GPUVAddr Address() const {
+                return static_cast<GPUVAddr>((static_cast<GPUVAddr>(address_high) << 32) |
+                                             address_low);
+            }
         };
         static_assert(sizeof(TransformFeedbackBinding) == 32);
 
@@ -644,6 +649,10 @@ public:
                 return true;
             }
             return shader_config[index].enable != 0;
+        }
+
+        bool IsShaderConfigEnabled(Regs::ShaderProgram type) const {
+            return IsShaderConfigEnabled(static_cast<std::size_t>(type));
         }
 
         union {
