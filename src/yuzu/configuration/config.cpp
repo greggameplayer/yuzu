fa7d81f2b279c90cf544c3d95467133eeea65b02
lat9nq <lat9nq@virginia.edu>
2020-07-03 13:36:37 -0400

    address reviewer concerns
    Changes function variables to const wherever they don't need to be changed. Sets Settings::Setting to final as it should not be inherited from. Sets ConfigurationShared::use_global_text to static.
    
    Co-Authored-By: VolcaEM <volcaem@users.noreply.github.com>

--- src/yuzu/configuration/config.cpp
+++ src/yuzu/configuration/config.cpp
@@ -1267,7 +1267,7 @@ QVariant Config::ReadSetting(const QString& name, const QVariant& default_value)
 
 template <typename Type>
 void Config::ReadSettingGlobal(Settings::Setting<Type>& setting, const QString& name) {
-    bool use_global = qt_config->value(name + QStringLiteral("/use_global"), true).toBool();
+    const bool use_global = qt_config->value(name + QStringLiteral("/use_global"), true).toBool();
     setting.SetGlobal(use_global);
     if (global || !use_global) {
         setting.SetValue(ReadSetting(name).value<Type>());
@@ -1277,7 +1277,7 @@ void Config::ReadSettingGlobal(Settings::Setting<Type>& setting, const QString& 
 template <typename Type>
 void Config::ReadSettingGlobal(Settings::Setting<Type>& setting, const QString& name,
                                const QVariant& default_value) {
-    bool use_global = qt_config->value(name + QStringLiteral("/use_global"), true).toBool();
+    const bool use_global = qt_config->value(name + QStringLiteral("/use_global"), true).toBool();
     setting.SetGlobal(use_global);
     if (global || !use_global) {
         setting.SetValue(ReadSetting(name, default_value).value<Type>());
@@ -1287,7 +1287,7 @@ void Config::ReadSettingGlobal(Settings::Setting<Type>& setting, const QString& 
 template <typename Type>
 void Config::ReadSettingGlobal(Type& setting, const QString& name,
                                const QVariant& default_value) const {
-    bool use_global = qt_config->value(name + QStringLiteral("/use_global"), true).toBool();
+    const bool use_global = qt_config->value(name + QStringLiteral("/use_global"), true).toBool();
     if (global || !use_global) {
         setting = ReadSetting(name, default_value).value<Type>();
     }
